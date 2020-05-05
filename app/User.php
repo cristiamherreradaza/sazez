a1bd50cee55f8677f7324feeefcd2fcbd497b29a
cristiam <cristiamherrera@gmail.com>
2020-05-05 10:13:23 -0400

    Se adiciono la clase para softdeletes

--- app/User.php
+++ app/User.php
@@ -2,9 +2,10 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
