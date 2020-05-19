8a2e9d62fef274c5eb5271f8ced44b007dd04994
cristiam <cristiamherrera@gmail.com>
2020-05-19 16:36:55 -0400

    Se creo la tabla de paquetes

---
+++ database/migrations/2020_05_19_162612_create_paquetes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaquetesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paquetes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('nombre', 80)->nullable();
+            $table->smallInteger('cantidad')->nullable()->default(0);
+            $table->string('estado', 30)->nullable();
+            $table->datetime('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paquetes');
+    }
+}
