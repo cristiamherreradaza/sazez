18a03bb1c5a3b3a26103a6e667ee1cfc35c08457
Ariel Fernandez <arielfernandez.rma7@gmail.com>
2020-06-04 19:23:09 -0400

    Corregido edición de combos

--- resources/views/combo/editar.blade.php
+++ resources/views/combo/editar.blade.php
@@ -114,9 +114,9 @@
                                                     ->first();
                                     @endphp
                                     <td>{{ $precio->precio }}</td>
-                                    <td><input type="number" class="form-control text-right precio" name="precio[`+id+`]" id="precio_`+id+`" value="{{ $producto->precio }}" data-id="`+id+`" step="any" min="1"></td>
-                                    <td><input type="number" class="form-control text-right cantidad" name="cantidad[`+id+`]" id="cantidad_`+id+`" value="{{ $producto->cantidad }}" data-id="`+id+`" min="1"></td>
-                                    <td><input type="number" class="form-control text-right subtotal" name="subtotal[`+id+`]" id="subtotal_`+id+`" value="{{ ($producto->precio*$producto->cantidad) }}" step="any" style="width: 120px;" readonly></td>
+                                    <td><input type="number" class="form-control text-right precio" name="precio[{{ $producto->producto_id }}]" id="precio_{{ $producto->producto_id }}" value="{{ $producto->precio }}" data-id="{{ $producto->producto_id }}" step="any" min="1"></td>
+                                    <td><input type="number" class="form-control text-right cantidad" name="cantidad[{{ $producto->producto_id }}]" id="cantidad_{{ $producto->producto_id }}" value="{{ $producto->cantidad }}" data-id="{{ $producto->producto_id }}" min="1"></td>
+                                    <td><input type="number" class="form-control text-right subtotal" name="subtotal[{{ $producto->producto_id }}]" id="subtotal_{{ $producto->producto_id }}" value="{{ ($producto->precio*$producto->cantidad) }}" step="any" style="width: 120px;" readonly></td>
                                     <td><button type="button" class="btnElimina btn btn-danger" title="Eliminar producto"><i class="fas fa-trash"></i></button></td>
                                     @php
                                         $total += ($producto->precio*$producto->cantidad)
@@ -180,6 +180,7 @@
     });
 
     $(document).on('keyup change', '.precio', function(e){
+        //alert('prueba');
         let precio = Number($(this).val());
         let id = $(this).data("id");
         let cantidad = Number($("#cantidad_"+id).val());
