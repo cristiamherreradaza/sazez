4a218acdb1bf9cafc79901ce80a43b1788300296
Ariel Fernandez <arielfernandez.rma7@gmail.com>
2020-06-16 13:23:38 -0400

    Ërror corregido

--- resources/views/email/cupon.blade.php
+++ resources/views/email/cupon.blade.php
@@ -5,10 +5,6 @@ Fuiste acreedor a un nuevo cupón de descuento del producto <a href="https://www
 
 @component('mail::promotion')
 
-<!-- <img src="data:image/png;base64, {{ base64_encode($qr) }} ">
-<img src="{!!$message->embedData($qr, 'QrCode.png', 'image/png')!!}"> -->
-
-<!-- <img src='data:image/png;base64, {{ base64_encode(QrCode::format("png")->color(34,82,162)->size(300)->generate($codigo)) }}'> -->
 <img src="{!!$message->embedData($codigo, 'QrCode.png', 'image/png')!!}">
 <br>
 Cupón valido hasta {{ $msg['fecha_final'] }}.<br>
