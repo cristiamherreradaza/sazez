83fb92cc2fe0fb0b87b1a3ce0aa9d05902589e1e
Ariel Fernandez <arielfernandez.rma7@gmail.com>
2020-06-16 14:05:22 -0400

    prueba de img

--- resources/views/email/prueba.blade.php
+++ resources/views/email/prueba.blade.php
@@ -1,2 +1,2 @@
 <h1>holas desde mi correo</h1>
-<img src='data:image/png;base64," . $png . "'>
+<img src="http://sazez.net/nuevo/public/assets/images/info.jpg" class="img-fluid rounded img-thumbnail" alt="informacion" />
