4fe934e0d1707710c9ebef4cff946550eaababe9
Ariel Fernandez <arielfernandez.rma7@gmail.com>
2020-07-29 14:48:03 -0400

    Se agrego migracion de proveedores, se agrego columna nueva en movimientos proveedor_id y se agrego/modifico modelos respectivos

---
+++ database/migrations/2020_07_29_133333_create_proveedores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProveedoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre', 120)->nullable();
+            $table->string('direccion', 255)->nullable();
+            $table->string('telefonos', 150)->nullable();
+            $table->string('estado', 30)->nullable();
+            $table->datetime('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedores');
+    }
+}
