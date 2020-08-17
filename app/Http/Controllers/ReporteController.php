b083f480bf526655705e3cc8e1a67a2cf3c34a03
Ariel Fernandez <arielfernandez.rma7@gmail.com>
2020-08-17 10:34:01 -0400

    Se agrego reporte de ingreso de productos por proveedores

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 use DataTables;
 use App\Almacene;
 use App\User;
+use App\Proveedore;
 
 class ReporteController extends Controller
 {
@@ -20,8 +21,6 @@ class ReporteController extends Controller
 
     public function ajaxVentasListado(Request $request)
     {
-        //dd($request->fecha_final);
-        
         $ventas = DB::table('ventas')
                     ->whereNull('ventas.deleted_at')
                     ->whereBetween('fecha', [$request->fecha_inicial, $request->fecha_final])
@@ -52,44 +51,38 @@ class ReporteController extends Controller
         return Datatables::of($ventas)->make(true);
     }
 
-    public function ajax_tienda_listado(Request $request)
-    {   
-        $almacen_id = $request->tipo_id;
-        $fecha_ini = $request->tipo_fecha_ini;
-        $fecha_fin = $request->tipo_fecha_fin;
-        if ($almacen_id == 0) {
-            $productos = DB::select("SELECT alma.nombre as alma_nombre, tipo.nombre as tipo_nombre, prod.nombre as prod_nombre, marc.nombre as marc_nombre, prod.colores, (SUM(movi.ingreso) - SUM(movi.salida)) as stock
-                                    FROM movimientos movi, almacenes alma, productos prod, tipos tipo, marcas marc
-                                    WHERE movi.almacene_id = alma.id
-                                    AND movi.producto_id = prod.id
-                                    AND prod.tipo_id = tipo.id
-                                    AND prod.marca_id = marc.id
-                                    AND fecha BETWEEN '$fecha_ini' AND '$fecha_fin'
-                                    AND movi.deleted_at IS NULL
-                                    GROUP BY movi.producto_id, alma.id");
-        } else {
-            $productos = DB::select("SELECT alma.nombre as alma_nombre, tipo.nombre as tipo_nombre, prod.nombre as prod_nombre, marc.nombre as marc_nombre, prod.colores, (SUM(movi.ingreso) - SUM(movi.salida)) as stock
-                                    FROM movimientos movi, almacenes alma, productos prod, tipos tipo, marcas marc
-                                    WHERE movi.almacene_id = '$almacen_id'
-                                    AND movi.almacene_id = alma.id
-                                    AND movi.producto_id = prod.id
-                                    AND prod.tipo_id = tipo.id
-                                    AND prod.marca_id = marc.id
-                                    AND fecha BETWEEN '$fecha_ini' AND '$fecha_fin'
-                                    AND movi.deleted_at IS NULL
-                                    GROUP BY movi.producto_id, alma.id");
+    public function proveedores()
+    {
+        $almacenes = Almacene::get();
+        $proveedores = Proveedore::get();
+        return view('reporte.proveedores')->with(compact('almacenes', 'proveedores'));
+    }
+
+    public function ajaxProveedoresListado(Request $request)
+    {
+        $productos = DB::table('movimientos')
+                    ->whereNull('movimientos.deleted_at')
+                    ->where('movimientos.estado', 'Ingreso')
+                    ->whereNotNull('proveedor_id')
+                    ->whereBetween('fecha', [$request->fecha_inicial, $request->fecha_final])
+                    ->leftJoin('proveedores', 'movimientos.user_id', '=', 'proveedores.id')
+                    ->leftJoin('almacenes', 'movimientos.almacene_id', '=', 'almacenes.id')
+                    ->leftJoin('productos', 'movimientos.producto_id', '=', 'productos.id')
+                    ->select(
+                        'movimientos.id as nro_envio',
+                        'almacenes.nombre as tienda',
+                        'proveedores.nombre as proveedor_nombre',
+                        'productos.codigo as codigo',
+                        'productos.nombre as producto_nombre',
+                        'movimientos.fecha as fecha',
+                        'movimientos.ingreso as cantidad'
+                    );
+        if($request->almacen_id){
+            $productos->where('movimientos.almacene_id', $request->almacen_id);
         }
-         return Datatables::of($productos)
-                ->make(true); 
-
-        // SELECT almacenes.nombre as alma_nombre, tipos.nombre as tipo_nombre, productos.nombre as prod_nombre, marcas.nombre as marc_nombre, productos.colores, (SUM(movimientos.ingreso) - SUM(movimientos.salida)) as stock
-        // FROM movimientos
-        // LEFT JOIN almacenes ON movimientos.almacene_id = almacenes.id
-        // LEFT JOIN productos ON movimientos.producto_id = productos.id
-        // LEFT JOIN tipos ON productos.tipo_id = tipos.id
-        // LEFT JOIN marcas ON productos.marca_id = marcas.id
-        // WHERE movimientos.fecha BETWEEN '$fecha_ini' AND '$fecha_fin'
-        // GROUP BY movimientos.producto_id, almacenes.id
-
+        if($request->proveedor_id){
+            $productos->where('movimientos.user_id', $request->proveedor_id);
+        }
+        return Datatables::of($productos)->make(true);
     }
 }
