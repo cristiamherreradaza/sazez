6abb10448aeff850f95a0316cee27a8ddbfd3595
cristiam <cristiamherrera@gmail.com>
2020-08-20 14:15:53 -0400

    Se construyeron las migraciones de empresa y parametros factura

---
+++ database/factories/ParametrosFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Parametros;
+use Faker\Generator as Faker;
+
+$factory->define(Parametros::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_08_20_111316_create_parametros_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParametrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parametros', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('numero_autorizacion', 150)->nullable();
+            $table->string('llave_dosificacion', 255)->nullable();
+            $table->integer('numero_factura')->nullable();
+            $table->date('fecha_limite')->nullable();
+            $table->string('estado', 30)->nullable();
+            $table->datetime('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parametros');
+    }
+}
