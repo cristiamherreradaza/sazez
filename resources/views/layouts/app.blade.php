0d12e84aeecc8462ef0f7b7291cec38e7b3fdb12
Ariel Fernandez <arielfernandez.rma7@gmail.com>
2020-09-03 19:30:27 -0400

    Se cambio notificacion de template a cupones recientes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,64 +104,50 @@
                                         <ul class="list-style-none">
                                             <li>
                                                 <div class="font-weight-medium border-bottom rounded-top py-3 px-4">
-                                                    Notifications
+                                                    Cupones Recientes
                                                 </div>
                                             </li>
                                             <li>
                                                 <div class="message-center notifications position-relative" style="height:250px;">
-                                                    <!-- Message -->
-                                                    <a href="javascript:void(0)"
-                                                        class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                                        <span class="btn btn-danger rounded-circle btn-circle"><i
-                                                                class="fa fa-link"></i></span>
-                                                        <div class="w-75 d-inline-block v-middle pl-2">
-                                                            <h5 class="message-title mb-0 mt-1">Luanch Admin</h5> <span
-                                                                class="font-12 text-nowrap d-block text-muted text-truncate">Just see
-                                                                the my new admin!</span> <span
-                                                                class="font-12 text-nowrap d-block text-muted">9:30 AM</span>
-                                                        </div>
-                                                    </a>
-                                                    <!-- Message -->
-                                                    <a href="javascript:void(0)"
-                                                        class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                                        <span class="btn btn-success rounded-circle btn-circle"><i
-                                                                class="ti-calendar"></i></span>
-                                                        <div class="w-75 d-inline-block v-middle pl-2">
-                                                            <h5 class="message-title mb-0 mt-1">Event today</h5> <span
-                                                                class="font-12 text-nowrap d-block text-muted text-truncate">Just a
-                                                                reminder that you have event</span> <span
-                                                                class="font-12 text-nowrap d-block text-muted">9:10 AM</span>
-                                                        </div>
-                                                    </a>
-                                                    <!-- Message -->
-                                                    <a href="javascript:void(0)"
-                                                        class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                                        <span class="btn btn-info rounded-circle btn-circle"><i
-                                                                class="ti-settings"></i></span>
-                                                        <div class="w-75 d-inline-block v-middle pl-2">
-                                                            <h5 class="message-title mb-0 mt-1">Settings</h5> <span
-                                                                class="font-12 text-nowrap d-block text-muted text-truncate">You can
-                                                                customize this template as you want</span> <span
-                                                                class="font-12 text-nowrap d-block text-muted">9:08 AM</span>
-                                                        </div>
-                                                    </a>
-                                                    <!-- Message -->
-                                                    <a href="javascript:void(0)"
-                                                        class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                                        <span class="btn btn-primary rounded-circle btn-circle"><i
-                                                                class="ti-user"></i></span>
-                                                        <div class="w-75 d-inline-block v-middle pl-2">
-                                                            <h5 class="message-title mb-0 mt-1">Pavan kumar</h5> <span
-                                                                class="font-12 text-nowrap d-block text-muted text-truncate">Just see
-                                                                the my admin!</span> <span
-                                                                class="font-12 text-nowrap d-block text-muted">9:02 AM</span>
-                                                        </div>
-                                                    </a>
+                                                    @php
+                                                        $cupones = App\Cupone::where('almacene_id', auth()->user()->almacen->id)
+                                                                            ->orWhere('almacene_id', NULL)
+                                                                            ->orderBy('id', 'desc')
+                                                                            ->take(5)
+                                                                            ->get();
+                                                    @endphp
+                                                    @foreach($cupones as $key => $cupon)
+                                                        <a href="javascript:void(0)" class="message-item d-flex align-items-center border-bottom px-3 py-2">
+                                                            @if($cupon->producto_id)
+                                                                <span class="btn btn-info rounded-circle btn-circle">
+                                                                    <i class="fas fa-cube"></i>
+                                                                </span>
+                                                            @else
+                                                                <span class="btn btn-info rounded-circle btn-circle">
+                                                                    <i class="fas fa-cubes"></i>
+                                                                </span>
+                                                            @endif
+                                                            
+                                                            <div class="w-75 d-inline-block v-middle pl-2">
+                                                                <h5 class="message-title mb-0 mt-1 text-info">{{ $cupon->user->name }}</h5>
+                                                                <span class="font-12 text-nowrap d-block text-muted text-truncate">
+                                                                    @if($cupon->producto_id)
+                                                                        {{ $cupon->producto->nombre }}
+                                                                    @else
+                                                                        {{ $cupon->combo->nombre }}
+                                                                    @endif
+                                                                </span>
+                                                                <span class="font-12 text-nowrap d-block text-danger">
+                                                                    {{ $cupon->fecha_final }}
+                                                                </span>
+                                                            </div>
+                                                        </a>
+                                                    @endforeach
                                                 </div>
                                             </li>
                                             <li>
                                                 <a class="nav-link border-top text-center text-dark pt-3" href="javascript:void(0);">
-                                                    <strong>Check all notifications</strong> <i class="fa fa-angle-right"></i> </a>
+                                                    <strong>Ver todos los cupones</strong> <i class="fa fa-angle-right"></i> </a>
                                             </li>
                                         </ul>
                                     </div>
