9da84a3c3e413a8fe9b9e2cfe73d73eee8a8945b
cristiam <cristiamherrera@gmail.com>
2020-10-06 13:16:27 -0400

    Se adiciono la ip

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -710,7 +710,32 @@ class VentaController extends Controller
         // $text = 'ignore everything except this (text)';
         preg_match('#\((.*?)\)#', $datosDispositivo, $match);
         print $match[1];
+        $ip = $this->getUserIP();
+        dd($ip);
 
         // dd($datosDispositivo);
     }
+
+    protected function getUserIP()
+    {
+        // Get real visitor IP behind CloudFlare network
+        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+            $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+        }
+        $client = @$_SERVER['HTTP_CLIENT_IP'];
+        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        $remote = $_SERVER['REMOTE_ADDR'];
+
+        if (filter_var($client, FILTER_VALIDATE_IP)) {
+            $ip = $client;
+        } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
+            $ip = $forward;
+        } else {
+            $ip = $remote;
+        }
+
+        return $ip;
+    }
+
 }
