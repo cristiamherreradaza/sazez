db00d6adddf7449496cd6dd8008b08a31f0696e0
cristiam <cristiamherrera@gmail.com>
2020-10-08 23:46:51 -0400

    Se adciono la tabla pedidos proveedores

--- database/migrations/2020_10_08_222547_create_pedidos_proveedores_table.php
+++ database/migrations/2020_10_08_222547_create_pedidos_proveedores_table.php
@@ -16,22 +16,16 @@ class CreatePedidosProveedoresTable extends Migration
         Schema::create('pedidos_proveedores', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id')->nullable();
-            // $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->unsignedBigInteger('almacene_id');
-            // $table->foreign('almacene_id')->references('id')->on('almacenes');
+            $table->foreign('almacene_id')->references('id')->on('almacenes');
             $table->unsignedBigInteger('proveedore_id');
-            // $table->foreign('proveedore_id')->references('id')->on('proveedores');
+            $table->foreign('proveedore_id')->references('id')->on('proveedores');
             $table->integer('numero')->nullable();
             $table->datetime('fecha')->nullable();
             $table->string('estado', 30)->nullable();
             $table->datetime('deleted_at')->nullable();
             $table->timestamps();
-        });
-
-        Schema::table('pedidos_proveedores', function($table){
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('almacene_id')->references('id')->on('almacenes');
-            $table->foreign('proveedore_id')->references('id')->on('proveedores');
         });
     }
 
