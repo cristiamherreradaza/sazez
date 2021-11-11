e35a8d0ec8d575def0052c237aa8378b7245f770
Joel Flores Quispe <jjjoelcito123@gmail.com>
2021-11-11 09:42:11 -0400

    Se quito el id del producto en saldos precios

--- resources/views/reporte/ajax_listado_saldos_precios.blade.php
+++ resources/views/reporte/ajax_listado_saldos_precios.blade.php
@@ -2,7 +2,7 @@
     <table id="tabla-usuarios" class="table table-striped table-bordered no-wrap">
         <thead>
             <tr>
-                <th>id</th>
+                {{-- <th>id</th> --}}
                 <th>Codigo</th>
                 <th>Producto</th>
                 <th>Tipo</th>
@@ -19,7 +19,7 @@
         <tbody>
             @foreach($productos as $producto)
                 <tr>
-                    <td>{{ $producto->id }}</td>
+                    {{-- <td>{{ $producto->id }}</td> --}}
                     <td>{{ $producto->codigo }}</td>
                     <td>{{ $producto->nombre }}</td>
                     <td>{{ $producto->tipo->nombre }}</td>
